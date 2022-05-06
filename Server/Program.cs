bf552a6c29e3dea84d825af0a3be71677c1dda75
AmirHosseinAkb <amirbvk1@gmail.com>
2022-05-06 20:30:25 +0430

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var webApplicationOptions = new WebApplicationOptions()
+{
+    EnvironmentName = Microsoft.Extensions.Hosting.Environments.Development
+};
+var builder = WebApplication.CreateBuilder(webApplicationOptions);
+builder.Services.AddRazorPages();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Errors/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+
+app.Run();
